57727d9d6f801f35f9b89426e7a2dc2e312e43d2
ubk8751 <ubk8751@gmail.com>
2021-03-27 14:46:27 +0100

    webbapp creation

--- app.py
+++ app.py
@@ -1,54 +1,12 @@
+# Import all the goodies
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import json
 import requests
 import csv
 
 file_exist = False
 
-def main():
-        
-        if file_exist:
-            items = if_file()
-        else:
-            items = if_requests()
-        
-        # Sort the dict and only take the information we need
-        sorted_items = create_dict(items)
-
-        # Export dict as json file
-        export_to_json(sorted_items)
-
-        # Export dict as csv file
-        export_to_csv(sorted_items)
-
-        print("Done")
-
-def if_requests():
-    # Get the requests file using requests.
-    request = requests.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/13.199167/lat/55.708333/data.json")
-    
-    # If the adress works, convert the requests file into a json file.
-    if request.status_code == 200:
-        items = request.json()
-
-        # Return our json file
-        return items
-
-# Get the Json file to read
-def if_file():
-    # Define the path to the file
-    path = "smhi_data.json"
-    
-    # Get the file
-    try:
-        f = open(path)
-        dct = json.load(f)
-        f.close()
-    
-        # Return the Json file
-        return dct
-    
-    except:
-        print("There was an error in loading the file!")
+app = Flask(__name__)
 
 # Get the relevant items and data and return as a dictionary
 def create_dict(items):
@@ -100,5 +58,66 @@ def export_to_json(items):
     except IOError:
         print("I/O error")
 
-if __name__ == "__main__":
-    main()
+@app.route("/", methods=["POST","GET"])
+def index():
+    if request.method == "POST":
+        if request.form['route'] == "link":
+            return redirect(url_for("if_link"))
+        if request.form['route'] == "file":
+            return redirect(url_for("if_file"))
+        else:
+            return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+# Route for getting a json file using a link, sort it and export as Json or CSV
+@app.route("/link", methods=["POST","GET"])
+def if_link():
+    # Get the requests file using requests.
+    request = requests.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/13.199167/lat/55.708333/data.json")
+    
+    # If the adress works, convert the requests file into a json file.
+    if request.status_code == 200:
+        items = request.json()
+
+        # Return our json file
+        return items
+
+# Route for reading a json file, sort it and export as Json or CSV
+@app.route("/file", methods=["POST","GET"])
+def if_file():
+    # Define the path to the file
+    path = "smhi_data.json"
+    
+    # Get the file
+    try:
+        f = open(path)
+        dct = json.load(f)
+        f.close()
+    
+        # Return the Json file
+        return dct
+    
+    except:
+        print("There was an error in loading the file!")
+
+'''def main():
+        
+        if file_exist:
+            items = if_file()
+        else:
+            items = if_link()
+        
+        # Sort the dict and only take the information we need
+        sorted_items = create_dict(items)
+
+        # Export dict as json file
+        export_to_json(sorted_items)
+
+        # Export dict as csv file
+        export_to_csv(sorted_items)
+
+        print("Done")'''
+
+'''if __name__ == "__main__":
+    main()'''
